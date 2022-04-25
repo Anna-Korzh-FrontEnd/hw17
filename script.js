9d469d26bea42db522abc3eab3a69b4f0c496557
kykyka <e.korj87@gmail.com>
2022-04-25 22:23:58 +0300

    fixes

--- script.js
+++ script.js
@@ -1,63 +1,61 @@
-"use strict";
+const input = document.getElementById("text")
+const listInput = document.getElementById("list")
+const btn = document.getElementById("submit")
+const errorMessage = document.querySelector(".error_message");
 
-//1:
-  const input = document.getElementById("text")
-  const listInput = document.getElementById("list")
-  const btn = document.getElementById("submit")
-  const errorMessage = document.querySelector(".error_message");
 
-  btn.onclick = function() {
-    event.preventDefault();
+input.onfocus = () => {
+  const isErrorField = input.classList.contains("error");
 
-  const inputText = input.value;
-  const inputItem = document.createElement("li");
-  inputItem.classList.add("itemLi");
-  
+  if (isErrorField) {
+    input.classList.remove("error");
+    errorMessage.innerHTML = "";
+  }
+};
+
+form.onsubmit = function() {
+  event.preventDefault();
   if(input.value.trim().length === 0){
     input.classList.add("error");
     errorMessage.innerHTML = "Пожалуйста, введите значение!";
     return
   }
-  input.onfocus = () => {
-      const isErrorField = input.classList.contains("error");
-    
-      if (isErrorField) {
-        input.classList.remove("error");
-        errorMessage.innerHTML = "";
-      }
-    };
 
-  inputItem.innerText = inputText;
-  const check = document.createElement("input");
-  check.type = "checkbox";
-  inputItem.prepend(check);
+  const inputText = input.value;
 
-
-  const disabled = function(event){
-    event.target.disabled = true
-    let li = event.target.closest('.itemLi');
-    li.classList.add('text-check');
-    li.getElementsByClassName('btn-Delete')[0].disabled = true;
-
-    }
-
-check.onchange = disabled;
+  const inputItem = addTodo(inputText);
 
   listInput.append(inputItem);
   input.value = "";
 
   const button = document.createElement("Button");
-  button.classList.add("btn-Delete");
+  button.classList.add("btnDelete");
   button.innerText = "Delete";
   inputItem.append(button);
-
-  inputItem.addEventListener("click", (event) =>{
-    const isRemoveButton = event.target.className === "btn-Delete";
-          
-          if(isRemoveButton){
-              const item = event.target.closest(".itemLi");
-              item.remove();
-          }
-  });
 }
 
+
+const addTodo = function(inputText) {
+  const inputItem = document.createElement("li");
+  inputItem.classList.add("itemLi");
+
+
+  inputItem.innerText = inputText;
+  const check = document.createElement("input");
+  check.type = "checkbox";
+  check.addEventListener("change", (event) =>{
+    setDisabled(event);
+  });
+
+  inputItem.prepend(check);
+
+  return inputItem;
+}
+
+const setDisabled = function(event){
+  event.target.disabled = true
+  let li = event.target.closest('.itemLi');
+  li.classList.add('text-check');
+  li.getElementsByClassName('btnDelete')[0].disabled = true;
+
+}
